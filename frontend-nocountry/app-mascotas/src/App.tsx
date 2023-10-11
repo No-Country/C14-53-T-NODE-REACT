343dd9396b5779d465c3a90bf71fa227669fbd55
Edwille <@baezedwille@gmail.com>
2023-10-10 22:48:59 -0400

    background sin responsive

--- frontend-nocountry/app-mascotas/src/App.tsx
+++ frontend-nocountry/app-mascotas/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import 'tailwindcss/tailwind.css'
 import Nav from '../components/Navbar/Navbar'
 import HomePage from '../components/HomePage'
+import Background from '../components/Background'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='min-h-screen'>
         <Nav></Nav>
         <HomePage></HomePage>
+        <Background></Background>
       </div>
     </>
   )
