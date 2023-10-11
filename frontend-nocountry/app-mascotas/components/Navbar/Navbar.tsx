d6aa2465aec5932775aec5155d32c4f687e3b5cb
David Martinez <dmz.mata@gmail.com>
2023-10-11 15:06:23 -0600

    Update router

--- frontend-nocountry/app-mascotas/components/Navbar/Navbar.tsx
+++ frontend-nocountry/app-mascotas/components/Navbar/Navbar.tsx
@@ -1,36 +1,37 @@
 import './navbar.css'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
     <>
       <nav className='container mx-auto'>
-        <div className='fixed left-0 w-full'>
+        <div className='fixed left-0 w-full z-[1000]'>
           <div className='max-w-screen-xl mx-auto p-4'>
             <div className='flex justify-between items-center h-16'>
-              <p className='logo'>PetCare</p>
+              <p className='logo'>
+                <Link to='/'>PetCare</Link>
+              </p>
               <ul className='flex items-center'>
                 <li>
-                  <a className='inline p-4 font-semibold hover:underline' href='#'>
-                    Sobre nosotros
-                  </a>
+                  <Link className='inline p-4 font-semibold hover:underline' to='/about'>
+                    Sobre Nosotros
+                  </Link>
                 </li>
+
                 <li>
-                  <a className='inline p-4 font-semibold hover:underline' href='#'>
+                  <Link className='inline p-4 font-semibold hover:underline' to='/contact-us'>
                     Contacto
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className='inline p-4 font-semibold hover:underline' href='#'>
+                  <Link className='inline p-4 font-semibold hover:underline' to='/login'>
                     Iniciar Sesion
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
-            
           </div>
-
         </div>
-
       </nav>
     </>
   )
--- frontend-nocountry/app-mascotas/src/App.tsx
+++ frontend-nocountry/app-mascotas/src/App.tsx
@@ -2,15 +2,23 @@ import './App.css'
 import 'tailwindcss/tailwind.css'
 import Nav from '../components/Navbar/Navbar'
 import HomePage from '../components/HomePage'
+import Login from '../components/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <div className='min-h-screen'>
-        <Nav></Nav>
-        <HomePage></HomePage>
-      </div>
-    </>
+    <Router>
+      <>
+        <div className='min-h-screen'>
+          <Nav></Nav>
+
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </>
+    </Router>
   )
 }
 
