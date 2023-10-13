9545ac33dfa1c2cc4f9e3fd28fcc3eb65da6d3ec
David Martinez <dmz.mata@gmail.com>
2023-10-13 01:55:07 -0600

    Update sidebar and login

--- frontend-nocountry/app-mascotas/src/components/Navbar/Navbar.tsx
+++ frontend-nocountry/app-mascotas/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
 
 function Nav() {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <>
       <nav className='container mx-auto'>
-        <div className='fixed left-0 w-full z-30 top-0'>
+        <div className='fixed left-0 w-full z-40 top-0'>
           <div className='max-w-screen-xl mx-auto py-4 px-3'>
             <div className='flex justify-between items-center'>
               <p className='logo'>
@@ -31,10 +34,40 @@ function Nav() {
                   </Link>
                 </li>
               </ul>
+              <div className={`hamburgerMenu cursor-pointer md:invisible z-40 ${toggle ? 'active' : ''}`} onClick={() => setToggle(!toggle)}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
             </div>
           </div>
         </div>
       </nav>
+      <div className={`fixed right-0 top-0 bg-beige min-h-full z-30 pt-24 w-3/6 transition-transform ease-in-out md:hidden ${toggle ? 'translate-x-0' : 'translate-x-full'}`}>
+        <ul className='md:hidden items-center'>
+          <li>
+            <Link className='p-4 font-semibold text-lg hover:underline block' to='/about'>
+              Sobre Nosotros
+            </Link>
+          </li>
+
+          <li>
+            <Link className='p-4 font-semibold text-lg hover:underline block' to='/contact-us'>
+              Contacto
+            </Link>
+          </li>
+          <li>
+            <Link className='p-4 font-semibold text-lg hover:underline block' to='/login'>
+              Iniciar Sesion
+            </Link>
+          </li>
+        </ul>
+        <p className='logo absolute bottom-[5%] text-center w-full'>
+          <Link className='p-4 block' to='/'>
+            PetCare
+          </Link>
+        </p>
+      </div>
     </>
   )
 }
