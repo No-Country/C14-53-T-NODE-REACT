8507e53893e4a4e7906d4a57e5abe6d3481a31c2
German <german.alejandrozulet@gmail.com>
2023-10-20 23:04:36 -0300

    creating panels and adjusting positions

--- frontend-nocountry/app-mascotas/src/components/Pets/PetAvatar.tsx
+++ frontend-nocountry/app-mascotas/src/components/Pets/PetAvatar.tsx
@@ -2,7 +2,7 @@ export const PetAvatar = ({ img, name }: { img: string; name: string }) => {
   return (
     <div className="xl:px-4 flex-shrink-0 flex flex-col items-center ">
       <img className='mb-2 w-[66px] h-[66px] md:w-[120px] md:h-[120px] xl:w-[150px] xl:h-[150px]' src={img} alt='{name}' />
-      <p className='md:text-[20px] text-[8px] text-center font-black font-montserray'>{name}</p>
+      <p className='md:text-[20px] text-sm text-center font-black font-montserray'>{name}</p>
     </div>
   )
 }
--- frontend-nocountry/app-mascotas/src/pages/MyPets.tsx
+++ frontend-nocountry/app-mascotas/src/pages/MyPets.tsx
@@ -3,27 +3,39 @@ import { PetAvatar } from "../components/Pets/PetAvatar"
 
 export const MyPets = () => {
   return (
-    <main >
-      <div className=' text-montserrat relative  h-[150px] md:h-[220px] xl:h-[260px]   w-[90%] lg:w-[60%] xl:w-[65%] md:m-8 md:px-12   mx-5 rounded-2xl opacity-100 shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
+    <main className="flex ">
+      <div className="flex flex-col items-center lg:items-stretch">
 
-
-        <h2 className='text-center py-2 font-black text-[1rem] xl:text-2xl'>Tus mascotas</h2>
-
-        <div className="flex items-center justify-around">
-          <div className="flex pl-2 md:pl-0 overflow-x-hidden py-2 w-[99%] lg:w-[89%] gap-3 lg:gap-6 xl:gap-3">
-
-            <PetAvatar img='./img/Chucky.png' name='Chucky'></PetAvatar>
-            <PetAvatar img='./img/Darth_Vader.png' name='Darth Vader'></PetAvatar>
-            <PetAvatar img='./img/Lucifer.png' name='Lucifer'></PetAvatar>
-            <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
-            <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
-            <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
-            <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
-
+        <div className=' text-montserrat relative  h-[160px] md:h-[220px] xl:h-[260px]   w-[85%] lg:w-[90%] xl:w-[90%] md:m-8 md:px-12    rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
+          <h2 className='text-center py-2 font-black text-[1rem] xl:text-2xl'>Tus mascotas</h2>
+          <div className="flex items-center justify-around">
+            <div className="flex pl-2 md:pl-0 overflow-x-hidden py-2 w-[99%] lg:w-[89%] gap-2 lg:gap-6 xl:gap-3">
+              <PetAvatar img='./img/Chucky.png' name='Chucky'></PetAvatar>
+              <PetAvatar img='./img/Darth_Vader.png' name='Darth Vader'></PetAvatar>
+              <PetAvatar img='./img/Lucifer.png' name='Lucifer'></PetAvatar>
+              <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
+              <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
+              <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
+              <PetAvatar img='./img/Juan.png' name='Juan'></PetAvatar>
+            </div>
+            <img className="hidden lg:flex pl-8 mb-12" src="../../public/img/arrow-right.svg" alt="carrousel-arrow-right" />
           </div>
-          <img className="hidden lg:flex pl-8 mb-12" src="../../public/img/arrow-right.svg" alt="carrousel-arrow-right" />
         </div>
+
+
+        {/* Luego hay que sacar el margen vertical que tengan, ya que estos seran componentes */}
+        <div className="h-[45vh] mt-8 lg:mt-0 relative w-[85%] lg:w-[90%] xl:w-[90%]  md:mx-8 rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]">
+        </div>
+
       </div>
+
+
+
+
+      <div className="hidden lg:flex h-[78vh]   m-8 pl-[150px]  relative  lg:w-[300px] xl:w-[25%]  rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]">
+
+      </div>
+
 
     </main>
   )
