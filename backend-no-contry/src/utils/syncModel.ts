ee7cd0278dc9167201c0c85f0a3e7045d728207d
Jhony A Saenz <jash0310@gmail.com>
2023-10-25 15:02:47 -0500

    Haciendo push para poder hacer merge

--- backend-no-contry/src/utils/syncModel.ts
+++ backend-no-contry/src/utils/syncModel.ts
@@ -4,9 +4,9 @@ import User from "../models/user.model";
 
 const syncModel = async () => {
   try {
-    await User.sync();
-    await Pet.sync();
-    await Calendar.sync()
+    await User.sync({ force: true });
+    await Pet.sync({ force: true });
+    await Calendar.sync({ force: true })
     console.log('Modelos sincronizados correctamente con la base de datos.');
   } catch (error) {
     console.error('Error al sincronizar el modelos con la base de datos:', error);
