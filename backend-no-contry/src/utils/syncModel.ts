19d44cdd32c57295df1d0468a64365dd0d631d65
Jhony A Saenz <jash0310@gmail.com>
2023-10-25 19:38:56 -0500

    Agregando token al response cuando se hace un registro 2

--- backend-no-contry/src/utils/syncModel.ts
+++ backend-no-contry/src/utils/syncModel.ts
@@ -5,10 +5,10 @@ import User from "../models/user.model";
 
 const syncModel = async () => {
   try {
-    await User.sync({ force: true });
-    await Pet.sync({ force: true });
-    await Calendar.sync({ force: true })
-    await MedicalRecord.sync({ force: true })
+    await User.sync({ alter: true });
+    await Pet.sync({ alter: true });
+    await Calendar.sync({ alter: true })
+    await MedicalRecord.sync({ alter: true })
     console.log('Modelos sincronizados correctamente con la base de datos.');
   } catch (error) {
     console.error('Error al sincronizar el modelos con la base de datos:', error);
