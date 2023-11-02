acd4331c527021ff58caf5ceb2f8ff07076d9d3d
German <german.alejandrozulet@gmail.com>
2023-11-02 12:26:40 -0300

    added endpoint to get events of a pet

--- backend-no-contry/src/controllers/calendar.controller.ts
+++ backend-no-contry/src/controllers/calendar.controller.ts
@@ -5,11 +5,14 @@ import { RequestExtends } from "../interfaces/reqExtends.interface";
 import Pet from "../models/petModel";
 
 const getCalendar = async (req: Request, res: Response) => {
+
+  const { petId } = req.params;
+
   try {
-    const getAllEvent = await findAllEvent();
+    const getAllEvent = await findAllEvent(petId);
     return res.status(200).json(getAllEvent);
   } catch (error) {
-    handleHttp(res, "Error al registrar el evento", error)
+    handleHttp(res, "Error al obtener los eventos", error)
   }
 }
 
