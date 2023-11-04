23e22a7442322ba90db800346bbccf9c990856dd
German <german.alejandrozulet@gmail.com>
2023-11-03 23:00:51 -0300

    medical records basic page

---
+++ frontend-nocountry/app-mascotas/src/pages/MedicalRecords.tsx
@@ -0,0 +1,14 @@
+
+export const MedicalRecords = () => {
+  return (
+    <div>
+
+      <div className='text-center py-3 md:py-0 md:ml-3 lg:ml-8 md:pr-3 rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
+        <div className='h-[150px] md:w-[180px] md:min-h-[500px] md:p-5 lg:min-w-[270px] 2xl:min-h-[700px] overflow-auto'>
+          <p className='text-[15px] my-2 md:mb-5 md:text-xl font-bold font-montserrat'>Tus Mascotas</p>
+          <div className='flex justify-center gap-3 md:gap-7 md:grid'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
