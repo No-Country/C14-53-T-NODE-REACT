d9af9597696d9e0125de9f143f59a599f05d747c
German <german.alejandrozulet@gmail.com>
2023-11-05 13:58:16 -0300

    removed mock up data

--- frontend-nocountry/app-mascotas/src/pages/MedicalRecords.tsx
+++ frontend-nocountry/app-mascotas/src/pages/MedicalRecords.tsx
@@ -15,13 +15,8 @@ export const MedicalRecords = () => {
         <div className='pb-3  text-center   lg:h-[600px] xl:h-[700px] overflow-auto'>
           <p className='text-lg my-2 lg:mb-5 lg:text-xl font-bold font-montserrat'>Tus Mascotas</p>
           <div className='flex justify-center gap-3 lg:gap-7 lg:grid px-6'>
-            <PetAvatar name={"Pedro"} fn={null} img={null}></PetAvatar>
-            <PetAvatar name={"Pedro"} fn={null} img={null}></PetAvatar>
-            <PetAvatar name={"Pedro"} fn={null} img={null}></PetAvatar>
-            <PetAvatar name={"Pedro"} fn={null} img={null}></PetAvatar>
-
-            {/* {MyPets ? MyPets?.slice(0, 6).map((pet: any, index: any) => <PetAvatar fn={null} key={index} img={pet.image} name={pet.name}></PetAvatar>) : <PetAvatarSkeleton /> */}
-            {/* } */}
+            {MyPets ? MyPets?.slice(0, 6).map((pet: any, index: any) => <PetAvatar fn={null} key={index} img={pet.image} name={pet.name}></PetAvatar>) : <PetAvatarSkeleton />
+            }
           </div>
         </div>
       </div>
