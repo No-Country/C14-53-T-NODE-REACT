3b58256bd2bbba964a33e0c29e6e8b4ff1860a7b
German <german.alejandrozulet@gmail.com>
2023-11-06 20:09:16 -0300

    added image and better message

--- frontend-nocountry/app-mascotas/src/pages/PageNotFound.tsx
+++ frontend-nocountry/app-mascotas/src/pages/PageNotFound.tsx
@@ -1,3 +1,12 @@
 export const PageNotFound = () => {
-  return <div>PageNotFound</div>
+  return <div className="flex justify-center flex-col gap-12 text-montserrat items-center h-screen">
+
+
+    <h1 className="font-black text-2xl md:text-5xl"> ¡UPS!</h1>
+    <p className="text-xl md:text-3xl font-semibold mb-12 ">¡No encontramos esta pagina! </p>
+
+
+    <img className="absolute bottom-0 z-20" src="./img/not-found.png" alt="not-found-image" />
+
+  </div>
 }
