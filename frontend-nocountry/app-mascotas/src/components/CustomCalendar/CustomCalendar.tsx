b612e33fddae644a0a55fc85014a60f8433d1472
German <german.alejandrozulet@gmail.com>
2023-11-10 20:06:29 -0300

    adjusted calendar for high resolutions

--- frontend-nocountry/app-mascotas/src/components/CustomCalendar/CustomCalendar.tsx
+++ frontend-nocountry/app-mascotas/src/components/CustomCalendar/CustomCalendar.tsx
@@ -25,19 +25,23 @@ const CustomCalendar: React.FC<CustomCalendarProps> = ({ events }) => {
   const MyPets = useGlobalStore(state => state.pets)
 
   return (
-    <div className='relative z-20 p-4 mx-4 mt-16 md:mt-20 2xl:flex 2xl:justify-center'>
-      <div className='mb-3 md:flex md:justify-between 2xl:w-2/3 2xl:mt-40'>
-        <div className='text-center py-3 md:py-0 md:ml-3 lg:ml-8 md:pr-3 rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
-          <div className='h-[150px] md:w-[180px] md:min-h-[500px] md:p-5 lg:min-w-[270px] 2xl:min-h-[700px] overflow-auto'>
+    <div className='relative z-20 p-4 h-screen  mx-4  2xl:my-0 2xl:flex 2xl:justify-center overflow-y-hidden '>
+
+
+      <div className='mb-3 md:flex mt-14 md:mt-20 md:justify-between 2xl:w-full  2xl:justify-around  '>
+
+        <div className='text-center   md:ml-3 lg:ml-8 2xl:ml-0 rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9] '>
+          <div className='h-[150px] md:w-[180px] md:h-[500px] md:p-5 lg:w-[180px] xl:w-[220px] 2xl:h-full overflow-auto'>
             <p className='text-[15px] my-2 md:mb-5 md:text-xl font-bold font-montserrat'>Tus Mascotas</p>
             <div className='flex justify-center gap-3 md:gap-7 md:grid'>{MyPets ? MyPets?.slice(0, 6).map((pet: any, index: any) => <PetAvatar fn={null} key={index} img={pet.image} name={pet.name}></PetAvatar>) : <PetAvatarSkeleton />}</div>
           </div>
         </div>
-        <div className='my-4 md:w-4/6 md:my-0 md:mx-6 lg:mx-20'>
-          <div className='p-5 md:p-7 md:min-h-[500px] md:px-10 rounded-2xl overflow-hidden shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
-            <div className='h-[300px] md:h-[400px] lg:h-[450px] 2xl:min-h-[700px]'>
-              <Calendar localizer={localizer} culture='es' events={events} startAccessor='start' endAccessor='end' style={{ background: '#F6F3E9' }} views={['month', 'week', 'day']} />
-            </div>
+
+
+        <div className='relative my-4 md:w-4/6 sacle  md:h-[90%] 2xl:w-[70vw]  md:my-0 md:mx-6 lg:mx-20 2xl:mx-0 2xl:p-12 lg:h-full  2xl:my-0 shadowbox containerbox 2xl:flex 2xl:items-center overflow-y-visible '>
+          <h1 className="text-montserrat text-center text-lg ml-2 lg:ml-0 font-bold py-2 lg:absolute  lg:top-[-32px] lg:left-[-20px] lg:uppercase lg:text-4xl xl:text-5xl lg:font-black">Calendario</h1>
+          <div className='h-[300px] md:h-[400px] p-4  lg:p-2 lg:h-[500px] 2xl:h-full 2xl:w-full '>
+            <Calendar localizer={localizer} culture='es' events={events} startAccessor='start' endAccessor='end' style={{ background: '#F6F3E9' }} views={['month', 'week', 'day']} />
           </div>
         </div>
       </div>
