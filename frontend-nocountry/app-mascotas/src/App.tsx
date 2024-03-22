12fbbb62eb7b1a07f4ba01879a0e5c84062263a1
German Soto <german@yop-dev.com>
2024-03-22 20:34:35 -0300

    Removing scroll for all app, formatting

--- frontend-nocountry/app-mascotas/src/App.tsx
+++ frontend-nocountry/app-mascotas/src/App.tsx
@@ -1,58 +1,58 @@
-import './App.css'
-import 'tailwindcss/tailwind.css'
-import Navbar from './components/Navbar/Navbar'
-import HomePage from './components/HomePage'
-import Login from './components/Login'
-import Register from './components/Register'
-import Dashboard from './components/Dashboard'
-import MyCalendar from './components/MyCalendar'
-import { PageNotFound } from './pages/PageNotFound'
-import { MyPets } from './pages/MyPets'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Background from './components/Background'
-import { Profile } from './components/Profile'
-import { ProtectedRoute } from './components/ProtectedRoute'
-import { ProtectedRouteAfterLogin } from './components/ProtectedRouteAfterLogin'
-import { useGlobalStore } from './store/globalStore'
-import { MedicalRecords } from './pages/MedicalRecords'
-import { Medicaments } from './pages/Medicaments'
-import { Birthdays } from './pages/Birthdays'
-import { Stadistics } from './pages/Stadistics'
+import "./App.css";
+import "tailwindcss/tailwind.css";
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import MyCalendar from "./components/MyCalendar";
+import { PageNotFound } from "./pages/PageNotFound";
+import { MyPets } from "./pages/MyPets";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Background from "./components/Background";
+import { Profile } from "./components/Profile";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { ProtectedRouteAfterLogin } from "./components/ProtectedRouteAfterLogin";
+import { useGlobalStore } from "./store/globalStore";
+import { MedicalRecords } from "./pages/MedicalRecords";
+import { Medicaments } from "./pages/Medicaments";
+import { Birthdays } from "./pages/Birthdays";
+import { Stadistics } from "./pages/Stadistics";
 
 function App() {
-  const isAuth = useGlobalStore(state => state.isAuth)
+  const isAuth = useGlobalStore((state) => state.isAuth);
 
   return (
     <Router>
       <>
-        <div className='relative'>
+        <div className="relative">
           <Navbar></Navbar>
 
           <Routes>
             <Route element={<ProtectedRouteAfterLogin isAllowed={isAuth} />}>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
             </Route>
 
             <Route element={<ProtectedRoute isAllowed={isAuth} />}>
-              <Route path='/calendar' element={<MyCalendar />} />
-              <Route path='/mypets' element={<MyPets />} />
-              <Route path='/medicaments' element={<Medicaments />} />
-              <Route path='/medicalrecords' element={<MedicalRecords />} />
-              <Route path='/birthdays' element={<Birthdays />} />
-              <Route path='/dashboard' element={<Dashboard />} />
-              <Route path='/stadistics' element={<Stadistics />} />
-              <Route path='/profile' element={<Profile />} />
+              <Route path="/calendar" element={<MyCalendar />} />
+              <Route path="/mypets" element={<MyPets />} />
+              <Route path="/medicaments" element={<Medicaments />} />
+              <Route path="/medicalrecords" element={<MedicalRecords />} />
+              <Route path="/birthdays" element={<Birthdays />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/stadistics" element={<Stadistics />} />
+              <Route path="/profile" element={<Profile />} />
             </Route>
 
-            <Route path='*' element={<PageNotFound />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
           </Routes>
           <Background />
         </div>
       </>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend-nocountry/app-mascotas/src/pages/MyPets.tsx
+++ frontend-nocountry/app-mascotas/src/pages/MyPets.tsx
@@ -1,226 +1,292 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { PetAvatar } from '../components/Pets/PetAvatar'
-import AddPetModal from '../components/modals/AddPetModal'
-import AddHistoryModal from '../components/modals/AddHistoryModal'
-import { useState, useEffect } from 'react'
-import SeeEventsModal from '../components/modals/SeeEventsModal'
-import AddActivityModal from '../components/modals/AddActivityModal'
-import { DeletePet, GetPets } from '../api/pets'
-import { PetAvatarSkeleton } from '../components/skeletons/PetAvatarSkeleton'
-import { useGlobalStore } from '../store/globalStore'
-import Swal from 'sweetalert2'
+import { PetAvatar } from "../components/Pets/PetAvatar";
+import AddPetModal from "../components/modals/AddPetModal";
+import AddHistoryModal from "../components/modals/AddHistoryModal";
+import { useState, useEffect } from "react";
+import SeeEventsModal from "../components/modals/SeeEventsModal";
+import AddActivityModal from "../components/modals/AddActivityModal";
+import { DeletePet, GetPets } from "../api/pets";
+import { PetAvatarSkeleton } from "../components/skeletons/PetAvatarSkeleton";
+import { useGlobalStore } from "../store/globalStore";
+import Swal from "sweetalert2";
 
 type Pet = {
-  name: string
-  surname: string
-  image: string
-  id?: string
-  userId: string
-  birthdate: string
-  species: string
-  breed: string
-  descriptions: string
-  weight: string
-}
+  name: string;
+  surname: string;
+  image: string;
+  id?: string;
+  userId: string;
+  birthdate: string;
+  species: string;
+  breed: string;
+  descriptions: string;
+  weight: string;
+};
 
 export const MyPets = () => {
-  const [showAddPetModal, setShowAddPetModal] = useState<boolean>(false)
-  const [showAddHistoryModal, setShowAddHistoryModal] = useState<boolean>(false)
-  const [showEventsModal, setShowEventsModal] = useState<boolean>(false)
-  const [showActivityModal, setShowActivityModal] = useState<boolean>(false)
-  const [selectedPet, setSelectedPet] = useState<Pet>()
-  const setMyPets = useGlobalStore(state => state.setPets)
-  const MyPets = useGlobalStore(state => state.pets)
-  const logout = useGlobalStore(state => state.logout)
+  const [showAddPetModal, setShowAddPetModal] = useState<boolean>(false);
+  const [showAddHistoryModal, setShowAddHistoryModal] =
+    useState<boolean>(false);
+  const [showEventsModal, setShowEventsModal] = useState<boolean>(false);
+  const [showActivityModal, setShowActivityModal] = useState<boolean>(false);
+  const [selectedPet, setSelectedPet] = useState<Pet>();
+  const setMyPets = useGlobalStore((state) => state.setPets);
+  const MyPets = useGlobalStore((state) => state.pets);
+  const logout = useGlobalStore((state) => state.logout);
 
   const Toast = Swal.mixin({
     toast: true,
-    position: 'bottom-end',
+    position: "bottom-end",
     showConfirmButton: false,
     timer: 3000,
     timerProgressBar: true,
-    didOpen: toast => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
 
   async function getPets() {
-    return await GetPets()
+    return await GetPets();
   }
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search)
-    const indexPet = Number(params.get('pet'))
+    const params = new URLSearchParams(window.location.search);
+    const indexPet = Number(params.get("pet"));
 
-    indexPet ? setPet(indexPet) : setPet(0)
-  }, [])
+    indexPet ? setPet(indexPet) : setPet(0);
+  }, []);
 
   useEffect(() => {
     getPets().then(
-      res => {
-        setMyPets(res.data?.pets)
+      (res) => {
+        setMyPets(res.data?.pets);
         // setPet(0)
       },
-      rej => {
-        if (rej.response.data == 'Sesión no valida') {
-          logout()
+      (rej) => {
+        if (rej.response.data == "Sesión no valida") {
+          logout();
         }
       }
-    )
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [showAddPetModal])
+  }, [showAddPetModal]);
 
   function setPet(index: number) {
-    if (MyPets === undefined) return
-    setSelectedPet(MyPets[index])
+    if (MyPets === undefined) return;
+    setSelectedPet(MyPets[index]);
   }
 
   function closePetModal() {
-    setShowAddPetModal(false)
-    document.body.style.overflowY = 'auto'
+    setShowAddPetModal(false);
+    document.body.style.overflowY = "auto";
   }
 
   function deletePet(id: string) {
     Swal.fire({
       title: `¿Estas seguro de que quieres eliminar a ${selectedPet?.name}?`,
-      text: 'No seras capaz de revertir esto',
-      icon: 'warning',
+      text: "No seras capaz de revertir esto",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, borrar!'
-    }).then(result => {
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, borrar!",
+    }).then((result) => {
       if (result.isConfirmed) {
-        const newPets = MyPets.filter((pet: any) => pet.id != id)
-        setMyPets(newPets)
+        const newPets = MyPets.filter((pet: any) => pet.id != id);
+        setMyPets(newPets);
 
-        DeletePet(id).then(res => {
-          if (res.status == 200 && res.data.message == 'pet removed') {
+        DeletePet(id).then((res) => {
+          if (res.status == 200 && res.data.message == "pet removed") {
             Toast.fire({
-              icon: 'success',
-              title: 'Mascota eliminada'
-            })
+              icon: "success",
+              title: "Mascota eliminada",
+            });
           }
-        })
+        });
       }
-    })
+    });
   }
 
   function openPetModal(e: React.MouseEvent<HTMLElement>): void {
-    e.preventDefault()
-    setShowAddPetModal(true)
+    e.preventDefault();
+    setShowAddPetModal(true);
   }
 
   return (
     <>
-      <main className='container mx-auto relative z-20 flex pt-[86px] md:pt-[150px]'>
-        <div className='flex flex-col items-center gap-8 lg:gap-0 z-90 lg:items-stretch'>
-          <div className=' text-montserrat relative  h-[160px] md:h-[220px] xl:h-[260px]   w-[85%] lg:w-[90%] xl:w-[95%] md:mx-8 md:mb-8 md:px-12    rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
-            <h2 className='text-center py-2 font-black text-[1rem] xl:text-2xl'>Tus mascotas</h2>
+      <main className="container mx-auto relative z-20 flex pt-[86px] md:pt-[80px] 2xl:pt-[150px]  ">
+        <div className="flex flex-col items-center gap-8 lg:gap-0 z-90 lg:items-stretch">
+          <div className=" text-montserrat relative  h-[160px] md:h-[220px] xl:h-[260px]   w-[85%] lg:w-[90%] xl:w-[95%] md:mx-8 md:mb-8 md:px-12    rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]">
+            <h2 className="text-center py-2 font-black text-[1rem] xl:text-2xl">
+              Tus mascotas
+            </h2>
             {MyPets.length ? (
               <>
-                <div className='flex items-center justify-between'>
-                  <a className='absolute left-2 top-[38%]' href='#firstslide'>
-                    <img className='hidden mb-12 rotate-180 lg:flex' src='./img/arrow-right.svg' alt='carrousel-arrow-right' />
+                <div className="flex items-center justify-between">
+                  <a className="absolute left-2 top-[38%]" href="#firstslide">
+                    <img
+                      className="hidden mb-12 rotate-180 lg:flex"
+                      src="./img/arrow-right.svg"
+                      alt="carrousel-arrow-right"
+                    />
                   </a>
-                  <div className='.n-scrollbar flex pl-2 md:pl-0 lg:pl-2 overflow-x-scroll lg:overflow-x-hidden scroll-smooth py-2 w-[99%] lg:w-[91%]  gap-2 lg:gap-6 xl:gap-3'>
-                    <div id='firstslide' className='flex gap-4'>
-                      {MyPets ? MyPets?.slice(0, 6).map((pet: any, index: any) => <PetAvatar fn={() => setPet(index)} key={index} img={pet.image} name={pet.name}></PetAvatar>) : <PetAvatarSkeleton />}
-                      <div id='anchor'></div>
+                  <div className=".n-scrollbar flex pl-2 md:pl-0 lg:pl-2 overflow-x-scroll lg:overflow-x-hidden scroll-smooth py-2 w-[99%] lg:w-[91%]  gap-2 lg:gap-6 xl:gap-3">
+                    <div id="firstslide" className="flex gap-4">
+                      {MyPets ? (
+                        MyPets?.slice(0, 6).map((pet: any, index: any) => (
+                          <PetAvatar
+                            fn={() => setPet(index)}
+                            key={index}
+                            img={pet.image}
+                            name={pet.name}
+                          ></PetAvatar>
+                        ))
+                      ) : (
+                        <PetAvatarSkeleton />
+                      )}
+                      <div id="anchor"></div>
                     </div>
                   </div>
-                  <a className='absolute right-2 top-[38%]' href='#anchor'>
-                    <img className='hidden mb-12 lg:flex' src='./img/arrow-right.svg' alt='carrousel-arrow-right' />
+                  <a className="absolute right-2 top-[38%]" href="#anchor">
+                    <img
+                      className="hidden mb-12 lg:flex"
+                      src="./img/arrow-right.svg"
+                      alt="carrousel-arrow-right"
+                    />
                   </a>
                 </div>
               </>
             ) : (
-              ''
+              ""
             )}
           </div>
 
-          <div className='h-[43vh] overflow-y-hidden flex flex-col  lg:flex-row  lg:gap-4 items-center py-6 lg:mt-0 text-montserrat relative w-[85%] lg:w-[90%] xl:w-[95%]  md:mx-8 rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
-            <button className='absolute lg:hidden right-8 '>
-              <img className='md:scale-150' src='./img/addtasks.svg' alt='add-task-button' />
+          <div className="h-[47vh] overflow-y-hidden flex flex-col  lg:flex-row  lg:gap-4 items-center py-6 lg:mt-0 text-montserrat relative w-[85%] lg:w-[90%] xl:w-[95%]  md:mx-8 rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]">
+            <button className="absolute lg:hidden right-8 ">
+              <img
+                className="md:scale-150"
+                src="./img/addtasks.svg"
+                alt="add-task-button"
+              />
             </button>
 
-            <img className='w-[100px] h-[100px] lg:w-[180px] lg:h-[180px] xl:h-[200px] xl:w-[330px] lg:ml-5 rounded-full xl:rounded-full   ' src={selectedPet?.image || './img/anonym-pet.jpeg'} alt='selected-pet-image' />
-            <div className='flex flex-col lg:gap-3'>
-              <div className='grid grid-cols-3 place-items-center m-4 gap-3  text-[10px] md:text-sm xl:text-xl'>
-                <div className='flex gap-1'>
-                  <h2 className='font-black'>Nombre:</h2>
-                  <p className='font-bold'>{selectedPet?.name}</p>
+            <img
+              className="w-[100px] h-[100px] lg:w-[180px] lg:h-[180px] xl:h-[200px] xl:w-[330px] lg:ml-5 rounded-full xl:rounded-full   "
+              src={selectedPet?.image || "./img/anonym-pet.jpeg"}
+              alt="selected-pet-image"
+            />
+            <div className="flex flex-col lg:gap-3">
+              <div className="grid grid-cols-3 place-items-center m-4 gap-3  text-[10px] md:text-sm xl:text-xl">
+                <div className="flex gap-1">
+                  <h2 className="font-black">Nombre:</h2>
+                  <p className="font-bold">{selectedPet?.name}</p>
                 </div>
-                <div className='flex gap-1'>
-                  <h2 className='font-black'>Especie:</h2>
-                  <p className='font-bold'>{selectedPet?.species}</p>
-                </div>
-
-                <div className='flex gap-1'>
-                  <h2 className='font-black'>Raza:</h2>
-                  <p className='font-bold whitespace-nowrap'>{selectedPet?.breed}</p>
-                </div>
-
-                <div className='flex gap-1'>
-                  <h2 className='font-black'>Peso:</h2>
-                  <p className='font-bold'>{selectedPet?.weight}</p>
+                <div className="flex gap-1">
+                  <h2 className="font-black">Especie:</h2>
+                  <p className="font-bold">{selectedPet?.species}</p>
                 </div>
 
-                <div className='flex gap-1 ml-20'>
-                  <h2 className='font-black whitespace-nowrap'>Fecha de nacimiento:</h2>
-                  <p className='font-bold'>{selectedPet?.birthdate}</p>
+                <div className="flex gap-1">
+                  <h2 className="font-black">Raza:</h2>
+                  <p className="font-bold whitespace-nowrap">
+                    {selectedPet?.breed}
+                  </p>
+                </div>
+
+                <div className="flex gap-1">
+                  <h2 className="font-black">Peso:</h2>
+                  <p className="font-bold">{selectedPet?.weight}</p>
+                </div>
+
+                <div className="flex gap-1 ml-20">
+                  <h2 className="font-black whitespace-nowrap">
+                    Fecha de nacimiento:
+                  </h2>
+                  <p className="font-bold">{selectedPet?.birthdate}</p>
                 </div>
               </div>
 
-              <div className='flex flex-col items-center gap-2'>
-                <h2 className='text-[12px] font-black md:text-sm xl:text-xl'>Descripcion Adicional</h2>
-                <p className='text-[10px] md:text-sm px-4 font-[700] mt-2  xl:text-lg'>{selectedPet?.descriptions}</p>
+              <div className="flex flex-col items-center gap-2">
+                <h2 className="text-[12px] font-black md:text-sm xl:text-xl">
+                  Descripcion Adicional
+                </h2>
+                <p className="text-[10px] md:text-sm px-4 font-[700] mt-2  xl:text-lg">
+                  {selectedPet?.descriptions}
+                </p>
               </div>
             </div>
           </div>
 
-          <div className='flex justify-center gap-7 md:justify-around  mobilepanel relative h-[70px] w-[85%] lg:hidden rounded-xl  shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
-            <button onClick={e => openPetModal(e)}>
-              <img src='./img/addpet.svg' alt='add-pet-button' />
+          <div className="flex justify-center gap-7 md:justify-around  mobilepanel relative h-[70px] w-[85%] lg:hidden rounded-xl  shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]">
+            <button onClick={(e) => openPetModal(e)}>
+              <img src="./img/addpet.svg" alt="add-pet-button" />
             </button>
             <button onClick={() => setShowAddHistoryModal(true)}>
-              <img src='./img/addhistory.svg' alt='add-history-button' />
+              <img src="./img/addhistory.svg" alt="add-history-button" />
             </button>
-            <button onClick={() => setShowEventsModal(true)} className='mb-2'>
-              <img src='./img/seetasks.svg' alt='see-tasks-button' />
+            <button onClick={() => setShowEventsModal(true)} className="mb-2">
+              <img src="./img/seetasks.svg" alt="see-tasks-button" />
             </button>
             <button>
-              <img src='./img/deletepet.svg' alt='delete-pet-button' />
+              <img src="./img/deletepet.svg" alt="delete-pet-button" />
             </button>
           </div>
         </div>
 
-        <div className='hidden lg:flex flex-col text-montserrat py-8 items-center gap-12 h-[68vh]   mb-8 mx-8  relative  lg:w-[90%] xl:w-[50%] 2xl:w-[40%]  rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]'>
-          <h2 className='text-3xl font-black xl:text-4xl'>Acciones</h2>
-          <ul className='font-bold text-left lg:text-lg xl:text-2xl'>
-            <li className='py-4'>
+        <div className="hidden lg:flex flex-col text-montserrat py-6 2xl:py-6 h-[85vh] 2xl:h-[79vh] items-center gap-12    mb-8 mx-8  relative  lg:w-[90%] xl:w-[50%] 2xl:w-[40%]  rounded-xl lg:rounded-2xl shadow-[0_0_38px_0_rgba(0,0,0,0.25)] bg-[#F6F3E9]">
+          <h2 className="text-3xl font-black xl:text-4xl">Acciones</h2>
+          <ul className="font-bold text-center lg:text-lg xl:text-2xl">
+            <li className="py-4">
               <button onClick={openPetModal}>Agregar mascota</button>
             </li>
-            <li className='py-4 xl:py-7'>
-              <button onClick={() => setShowAddHistoryModal(true)}>Ingresar historia medica </button>
+            <li className="py-4 xl:py-7">
+              <button onClick={() => setShowAddHistoryModal(true)}>
+                Ingresar historia medica{" "}
+              </button>
             </li>
-            <li className='py-4 xl:py-7'>
-              <button onClick={() => setShowActivityModal(true)}>Agendar Actividad</button>
+            <li className="py-4 xl:py-7">
+              <button onClick={() => setShowActivityModal(true)}>
+                Agendar Actividad
+              </button>
             </li>
-            <li className='py-4 xl:py-7'>
-              <button onClick={() => setShowEventsModal(true)}>Ver Actividades</button>
+            <li className="py-4 xl:py-7">
+              <button onClick={() => setShowEventsModal(true)}>
+                Ver Actividades
+              </button>
             </li>
-            <li className='py-4 xl:py-7'>{selectedPet ? <button onClick={() => deletePet(selectedPet.id!)}>Eliminar</button> : ''}</li>
+            <li className="py-4 xl:py-7">
+              {selectedPet ? (
+                <button onClick={() => deletePet(selectedPet.id!)}>
+                  Eliminar
+                </button>
+              ) : (
+                ""
+              )}
+            </li>
           </ul>
         </div>
 
-        <AddPetModal isVisible={showAddPetModal} onClose={() => closePetModal()} />
+        <AddPetModal
+          isVisible={showAddPetModal}
+          onClose={() => closePetModal()}
+        />
 
-        <AddHistoryModal isVisible={showAddHistoryModal} onClose={() => setShowAddHistoryModal(false)} />
-        <SeeEventsModal isVisible={showEventsModal} petInfo={selectedPet} onClose={() => setShowEventsModal(false)} />
-        <AddActivityModal isVisible={showActivityModal} petInfo={selectedPet} onClose={() => setShowActivityModal(false)} />
+        <AddHistoryModal
+          isVisible={showAddHistoryModal}
+          onClose={() => setShowAddHistoryModal(false)}
+        />
+        <SeeEventsModal
+          isVisible={showEventsModal}
+          petInfo={selectedPet}
+          onClose={() => setShowEventsModal(false)}
+        />
+        <AddActivityModal
+          isVisible={showActivityModal}
+          petInfo={selectedPet}
+          onClose={() => setShowActivityModal(false)}
+        />
       </main>
     </>
-  )
-}
+  );
+};
